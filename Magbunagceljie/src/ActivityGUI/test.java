009152bf96461404f032fae92cc9b1e57482f030
Cjieeey <105692921+cjieey@users.noreply.github.com>
2023-11-13 15:21:26 +0800

    Add files via upload

---
+++ Magbunagceljie/src/ActivityGUI/test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ActivityGUI;
+
+/**
+ *
+ * @author WS
+ */
+public class test {
+    public static void main(String[]args){
+        System.out.print(6.1+0.6);
+    }
+}
