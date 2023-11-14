90eea1e3c4744f08f5538c1dac2ab1689f86cbea
Cjieeey <105692921+cjieey@users.noreply.github.com>
2023-11-15 00:43:28 +0800

    Add files via upload

---
+++ NEXTCAL-main/Magbunagceljie/src/ActivityGUI/test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ActivityGUI;
+
+/**
+ *
+ * @author WS
+ */
+public class test {
+    public static void main(String[]args){
+        double n = 5;
+        System.out.print(((5+(-5.23))-1)*2);
+    }
+}
